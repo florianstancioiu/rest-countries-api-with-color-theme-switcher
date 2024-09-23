38f302fcf861ea23599bdab6acb2cc69a5657fa2
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2024-09-23 11:42:53 +0300

    feat: removed sketch:type attributes

--- src/router.tsx
+++ src/router.tsx
@@ -2,17 +2,20 @@ import { createBrowserRouter } from 'react-router-dom';
 import Default from './pages/Default';
 import Home from './pages/Home';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Default />,
-    children: [
-      {
-        path: '',
-        element: <Home />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Default />,
+      children: [
+        {
+          path: '',
+          element: <Home />,
+        },
+      ],
+    },
+  ],
+  { basename: '/rest-countries-api-with-color-theme-switcher/' }
+);
 
 export default router;
